f95a19333d02efd9e9c49665cb5c505292cfbdb3
Seltaeb87 <bastiaanbitter87@gmail.com>
2014-11-18 15:41:12 +0100

    Finished lesson 18: Assignment Operators

--- 18_AssignmentOperators/practice.php
+++ 18_AssignmentOperators/practice.php
@@ -1,17 +1,18 @@
 <?php
 	
 	// Constants
-
+	define('TITLE', 'Assignment Operator');
 	
 	// Custom Variables
-
+	$lesson_num = 18;
+	$name = 'Bastiaan';
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP <?php echo TITLE; ?></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -20,7 +21,7 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lesson_num; ?>: <small><?php echo TITLE; ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
@@ -29,32 +30,56 @@
 				
 				<h3>Addition Assignment Operator <code>+=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a += $b; //Adds $a and $b together
+
+					echo $a;
 				?>
 				
 				<h3>Subtraction Assignment Operator <code>-=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a -= $b; //Subtract $b(50) from $a(30)
+
+					echo $a;
 				?>
 				
 				<h3>Multiplication Assignment Operator <code>*=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a *= $b;
+
+					echo $a;
 				?>
 				
 				<h3>Division Assignment Operator <code>/=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a /= $b;
+
+					echo $a;
 				?>
 				
 				<h3>Modulus Assignment Operator <code>%=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a %= $b;
+
+					echo $a;
 				?>
 				
 				<h3>Concatenation Assignment Operator <code>.=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a .= $b; //will output 3050
+
+					echo $a;
 				?>
 				
 			</div><!-- end sandbox -->
@@ -63,7 +88,7 @@
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
+			<small>&copy;<?php echo date('Y'); ?>- <?php echo $name; ?></small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">

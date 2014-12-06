da574d031fb5489d01b669c12028dc2a2de6acf1
Seltaeb87 <bastiaanbitter87@gmail.com>
2014-12-06 12:04:44 +0100

    First part of the final project: Code a basic webpage layout

---
+++ 26_Final/Student/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="/assets/styles.css">
+</head>
+
+<body id='final-example'>
+
+	<div class='wrapper'>
+
+		<div id='banner'>
+			<a href='/' title='Home'>
+				<img src='img/banner.png' alt='Bas' fine dining>
+			</a>
+		</div><!-- banner -->
+
+		<div id='nav'>
+			<ul>
+				<li><a href='index.php'>Home</a></li>
+				<li><a href='team.php'>Team</a></li>
+				<li><a href='menu.php'>Menu</a></li>
+				<li><a href='contact.php'>Contact</a></li>
+			</ul
+		</div><!-- nav -->
+
+		<div class='content'>
+
+			<div id='footer' class='cf'>
+
+				<div class='column three'>
+					<strong>Phone:</strong>
+					035-1234567
+				</div><!-- column three-->
+
+				<div class='column three'>
+					<strong>Location:</strong>
+					Hilversumestraatweg 123
+					Hilversum, NL
+				</div><!-- column three-->
+
+				<div class='column three last'>
+
+					<strong>Hours:</strong>
+					<em>Dinsdag - Donderdag</em><br>
+					13:00 - 21:00<br><br>
+
+					<em>Vrijdag - Zaterdag</em><br>
+					16:00 - 23:00<br><br>
+
+					<em>Zondag - Maandag</em><br>
+					Closed<br><br>
+
+				</div><!-- column last-->
+
+			</div><!-- footer -->
+
+			<small>&copy; 2014 Bastiaan</small>
+
+		</div><!-- content -->
+
+	</div><!-- wrapper -->
+
+	<div class='copyright-info'>
+		<?php include('../assets/includes/copyright.php'); ?>
+	</div><!-- copyright-info -->
+
+</body>
+</html

5e1faad2ff4a60397b67568b21ff047a1f957745
Seltaeb87 <bastiaanbitter87@gmail.com>
2014-12-07 13:58:44 +0100

    added includes folder with header, nav, array and footer files

---
+++ 26_Final/Student/includes/footer.php
@@ -0,0 +1,42 @@
+
+			<div id='footer' class='cf'>
+
+				<div class='column three'>
+					<strong>Phone:</strong>
+					035-1234567
+				</div><!-- column three-->
+
+				<div class='column three'>
+					<strong>Location:</strong>
+					Hilversumestraatweg 123
+					Hilversum, NL
+				</div><!-- column three-->
+
+				<div class='column three last'>
+
+					<strong>Hours:</strong>
+					<em>Dinsdag - Donderdag</em><br>
+					13:00 - 21:00<br><br>
+
+					<em>Vrijdag - Zaterdag</em><br>
+					16:00 - 23:00<br><br>
+
+					<em>Zondag - Maandag</em><br>
+					Closed<br><br>
+
+				</div><!-- column last-->
+
+			</div><!-- footer -->
+
+			<small>&copy; 2014 Bastiaan</small>
+
+		</div><!-- content -->
+
+	</div><!-- wrapper -->
+
+	<div class='copyright-info'>
+		<?php include('../assets/includes/copyright.php'); ?>
+	</div><!-- copyright-info -->
+
+</body>
+</html
---
+++ 26_Final/Student/includes/header.php
@@ -0,0 +1,26 @@
+<?php
+	include('includes/arrays.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="/assets/styles.css">
+</head>
+
+<body id='final-example'>
+
+	<div class='wrapper'>
+
+		<div id='banner'>
+			<a href='/' title='Home'>
+				<img src='img/banner.png' alt='Bas fine dining'>
+			</a>
+		</div><!-- banner -->
+
+		<div id='nav'>
+			<?php include('includes/nav.php');?>
+		</div><!-- nav -->
+
+		<div class='content'>

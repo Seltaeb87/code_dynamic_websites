44bf216324745dfdbd11458ef062bae02e170a20
Seltaeb87 <bastiaanbitter87@gmail.com>
2014-12-12 19:29:01 +0100

    Added the first functionalities for sending a mail

--- 26_Final/Student/contact.php
+++ 26_Final/Student/contact.php
@@ -8,6 +8,35 @@ include('includes/header.php');
 	<div id="contact">
 		<hr>
 		<h1>Get in touch with us!</h1>
+
+		<?php 
+
+			//check for header injections
+			function had_header_injection($str){
+				return preg_match( "/[\r\n]/", $str );
+			}
+
+			if (isset($_POST['contact_submit'])) {
+
+				$name    = trim($_POST['name']);
+				$email   = trim($_POST['email']);
+				$msg     = $_POST['message'];
+
+				//check to see if name or email have header injections
+				if (has_header_injection($name) || has_header_injection($email)) {
+					die(); //if true, kill the script
+				}
+
+					if (!$name || !$email || $msg) {
+						echo '<h4 class="error">All fiels required.</h4><a href="contact.php class="button block>Go back and try again</a>'
+					}	exit;
+
+
+
+			}
+
+		 ?>
+
 			<form method ="post" action="" id="contact-form">
 
 				<label for="name">Your name:</label>
